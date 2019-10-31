4557d2a42dcc8b8b4a1d4a29d8877a5b3177576b
Troels Sheepyhead Jessen <kairyuka@gmail.com>
2019-10-31 14:17:51 +0100

    Made a sprite spin!

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,49 @@
-extern crate piston;
-extern crate graphics;
+extern crate find_folder;
 extern crate glutin_window;
+extern crate graphics;
 extern crate opengl_graphics;
+extern crate piston;
 
-use piston::window::WindowSettings;
+use glutin_window::GlutinWindow as Window;
+use graphics::rectangle::square;
+use opengl_graphics::{Filter, GlGraphics, OpenGL, Texture, TextureSettings};
 use piston::event_loop::*;
 use piston::input::*;
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{ GlGraphics, OpenGL };
+use piston::window::WindowSettings;
 
-pub struct App {
-    gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64   // Rotation for the square.
+struct App {
+    pub gl: GlGraphics,                   // OpenGL drawing backend.
+    pub rotation: f64,                    // Rotation for the sprite.
+    pub asset_folder: std::path::PathBuf, // Path to assets
 }
 
 impl App {
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
 
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 
-        let square = rectangle::square(0.0, 0.0, 50.0);
+        let sprite = self.asset_folder.join("sprite.png");
+        let image = Image::new().rect(square(0.0, 0.0, 50.0));
+        let texture_settings = TextureSettings::new()
+            .filter(Filter::Nearest)
+            .mipmap(Filter::Nearest);
+        let sprite = Texture::from_path(sprite,&texture_settings).unwrap();
         let rotation = self.rotation;
-        let (x, y) = (args.window_size[0] / 2.0,
-                      args.window_size[1] / 2.0);
+        let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
-            clear(GREEN, gl);
+            clear(BLACK, gl);
 
-            let transform = c.transform.trans(x, y)
-                                       .rot_rad(rotation)
-                                       .trans(-25.0, -25.0);
+            let transform = c
+                .transform
+                .trans(x, y)
+                .rot_rad(rotation)
+                .trans(-25.0, -25.0);
 
             // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
+            image.draw(&sprite, &DrawState::default(), transform, gl);
         });
     }
 
@@ -50,10 +58,7 @@ fn main() {
     let opengl = OpenGL::V3_2;
 
     // Create an Glutin window.
-    let mut window: Window = WindowSettings::new(
-            "spinning-square",
-            [200, 200]
-        )
+    let mut window: Window = WindowSettings::new("spinning-square", [200, 200])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
@@ -62,7 +67,10 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        rotation: 0.0
+        rotation: 0.0,
+        asset_folder: find_folder::Search::ParentsThenKids(3, 3)
+            .for_folder("assets")
+            .unwrap(),
     };
 
     let mut events = Events::new(EventSettings::new());
