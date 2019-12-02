afd11750cd34c996ea2d0f4cfaee1bf49ca85afa
Troels Sheepyhead Jessen <kairyuka@gmail.com>
2019-12-02 22:06:07 +0100

    Implemented input mapping struct and corresponding structs, next put them in a resource and make a system for them

--- src/engine/lib.rs
+++ src/engine/lib.rs
@@ -6,6 +6,7 @@ use ggez::event::EventHandler;
 use ggez::event::EventsLoop;
 use ggez::graphics;
 pub use ggez::graphics::FilterMode;
+use ggez::input::keyboard::*;
 use ggez::timer;
 use ggez::Context;
 use ggez::ContextBuilder;
@@ -14,6 +15,8 @@ use resources::*;
 pub use specs::world::Builder;
 use specs::*;
 pub use specs::{Entity, EntityBuilder};
+use std::cmp::Eq;
+use std::collections::HashMap;
 use systems::*;
 pub use uuid::Uuid;
 
@@ -21,7 +24,12 @@ pub mod components;
 pub mod resources;
 pub mod systems;
 
-pub struct GameState<'a, 'b>(ECS<'a, 'b>, Context, EventsLoop);
+pub struct GameState<'a, 'b> {
+    ecs: ECS<'a, 'b>,
+    context: Context,
+    event_loop: EventsLoop,
+    keyboard_maps: KeyboardMaps,
+}
 
 pub struct ECS<'a, 'b> {
     world: World,
@@ -36,6 +44,7 @@ fn register_components(world: &mut World) {
 
 fn insert_resources(world: &mut World) {
     world.insert(DeltaTime(0.0));
+    world.insert(None::<InputContext>);
 }
 
 pub fn new_game_state(title: &str, size: (f32, f32)) -> GameState {
@@ -60,7 +69,12 @@ pub fn new_game_state(title: &str, size: (f32, f32)) -> GameState {
     let dispatcher = DispatcherBuilder::new()
         .with(UpdatePos, "update_pos", &[])
         .build();
-    GameState(ECS { world, dispatcher }, context, event_loop)
+    GameState {
+        ecs: ECS { world, dispatcher },
+        context,
+        event_loop,
+        keyboard_maps: KeyboardMaps::new(),
+    }
 }
 
 impl EventHandler for ECS<'_, '_> {
@@ -69,6 +83,13 @@ impl EventHandler for ECS<'_, '_> {
             // Scoped so the pointer is thrown out as soon as it's no longer useful
             let mut delta = self.world.write_resource::<DeltaTime>();
             *delta = DeltaTime(timer::delta(context).as_secs_f64());
+        }
+        {
+            let mut input_context = self.world.write_resource::<InputContext>();
+            *input_context = InputContext {
+                keyboard_context: context.keyboard_context.clone(),
+                mouse_context: context.mouse_context.clone(),
+            }
         }
 
         self.dispatcher.dispatch(&mut self.world);
@@ -89,13 +110,13 @@ impl EventHandler for ECS<'_, '_> {
 }
 
 pub fn create_entity<'a>(
-    GameState(game, _, _): &'a mut GameState,
+    game_state: &'a mut GameState,
     x: f64,
     y: f64,
     size: f64,
     rotation: f64,
 ) -> EntityBuilder<'a> {
-    game.world.create_entity().with(Transform {
+    game_state.ecs.world.create_entity().with(Transform {
         x,
         y,
         size,
@@ -103,13 +124,74 @@ pub fn create_entity<'a>(
     })
 }
 
-pub fn run(GameState(game, context, event_loop): &mut GameState) {
-    match event::run(context, event_loop, game) {
+pub fn run(game_state: &mut GameState) {
+    match event::run(
+        &mut game_state.context,
+        &mut game_state.event_loop,
+        &mut game_state.ecs,
+    ) {
         Ok(_) => println!("Game exited cleanly"),
         Err(e) => println!("Error occurred: {}", e),
     }
 }
 
-pub fn load_image(GameState(_, context, _): &mut GameState, filename: &str) -> graphics::Image {
-    graphics::Image::new(context, filename).expect(&format!("Failed loading image with file name: {}", filename))
+pub fn load_image(game_state: &mut GameState, filename: &str) -> graphics::Image {
+    graphics::Image::new(&mut game_state.context, filename).expect(&format!(
+        "Failed loading image with file name: {}",
+        filename
+    ))
 }
+
+pub fn add_button_down_mapping(
+    game_state: &mut GameState,
+    key_code: KeyCode,
+    key_mods: KeyMods,
+    function: fn(&Transform),
+) {
+    game_state
+        .keyboard_maps
+        .button_down
+        .push(KeyboardMapping(key_code, key_mods, function))
+}
+
+pub fn add_button_up_mapping(
+    game_state: &mut GameState,
+    key_code: KeyCode,
+    key_mods: KeyMods,
+    function: fn(&Transform),
+) {
+    game_state
+        .keyboard_maps
+        .button_up
+        .push(KeyboardMapping(key_code, key_mods, function))
+}
+
+pub fn add_button_repeat_mapping(
+    game_state: &mut GameState,
+    key_code: KeyCode,
+    key_mods: KeyMods,
+    function: fn(&Transform),
+) {
+    game_state
+        .keyboard_maps
+        .button_repeat
+        .push(KeyboardMapping(key_code, key_mods, function))
+}
+
+pub struct KeyboardMaps {
+    pub button_down: Vec<KeyboardMapping>,
+    pub button_up: Vec<KeyboardMapping>,
+    pub button_repeat: Vec<KeyboardMapping>,
+}
+
+impl KeyboardMaps {
+    fn new() -> KeyboardMaps {
+        KeyboardMaps {
+            button_down: Vec::new(),
+            button_up: Vec::new(),
+            button_repeat: Vec::new(),
+        }
+    }
+}
+
+pub struct KeyboardMapping(KeyCode, KeyMods, fn(&Transform));
--- src/engine/resources.rs
+++ src/engine/resources.rs
@@ -1,2 +1,11 @@
+use ggez::input::keyboard::KeyboardContext;
+use ggez::input::mouse::MouseContext;
+
 #[derive(Default)]
 pub struct DeltaTime(pub f64);
+
+#[derive(Default)]
+pub struct InputContext {
+    pub keyboard_context: KeyboardContext,
+    pub mouse_context: MouseContext,
+}
